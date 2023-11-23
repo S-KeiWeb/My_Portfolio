40936d729b363ee96c08c15f22179a828e621c7b
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2023-11-23 15:15:57 +0900

    jsを読み込ませる処理完了

--- functions.php
+++ functions.php
@@ -9,7 +9,9 @@
     return $separator;
   }
 
+add_action( 'wp_enqueue_scripts', 'my_plugin_assets' );
+function my_plugin_assets() {
   wp_enqueue_script('jquery');
   wp_enqueue_script('portfolio-main', get_template_directory_uri() . '/js/app.js');
-
+}
 
--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@
 
 })(document);
 
-jquery(function (){
-  jquery('.openbtn').click(function () {
-    jquery(this).toggleClass('active');
-    jquery('#mask, #mask-menu').toggleClass('show');
+jQuery(function (){
+  jQuery('.openbtn').click(function () {
+    jQuery(this).toggleClass('active');
+    jQuery('#mask, #mask-menu').toggleClass('show');
   });
 });
