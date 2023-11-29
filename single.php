9b903faa57cfcd6796e5f72b3428da430b36d7ce
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2023-11-29 13:02:03 +0900

    投稿ページの表示を完了

--- single.php
+++ single.php
@@ -1,32 +1,19 @@
-<?php get_header(); ?>  
-
-  <main class="page-wrap">
-    <h1 class="page-title">
-        <span class="title-en">Works</span>
-        <span class="title-ja">制作物について</span>
-    </h1>
-    <div class="works-page-wrap">
-      <section class="works-content-wrap">
-        <h2 class="works-content-title">NEXTRISE</h2>
-        <img class="works-page-image" src="img/nextrise-hp-top.png" alt="NEXTRISEホームページ画像">
-        <dl class="works-list-wrap">
-          <dt class="works-list-title">URL</dt>
-          <dd class="works-list-item"><a href="https:nextrise-em.com" target="_blank">https://nextrise-em.com</a></dd>
-          <dt class="works-list-title">制作概要</dt>
-          <dd class="works-list-item">埼玉県越谷市で蓄電池販売を行っているNEXTRISE様のホームページ制作</dd>
-          <dt class="works-list-title">制作目的</dt>
-          <dd class="works-list-item">見込み客への、蓄電池説明のためと、ブラウザで検索されたときに、検索結果に出てくることで安心感を持ってもらうことを目的に作成依頼がありました。</dd>
-          <dt class="works-list-title">デザイン</dt>
-          <dd class="works-list-item">主に蓄電池の説明が重要とのことでしたので、それを説明するページに関しては見やすさを意識して制作しました。</dd>
-          <dt class="works-list-title">使用技術</dt>
-          <dd class="works-list-item">WordPress・Photoshop</dd>
-          <dt class="works-list-title">制作期間</dt>
-          <dd class="works-list-item">制作：２ヶ月</dd>
-          <dt class="works-list-title">課題・解決策</dt>
-          <dd class="works-list-item">初めてのホームページ制作でした。<br>クライアントが既存のサイトを持っていなかったため、ドメインとサーバーの契約からサポートしました。<br>この過程で、初めての試みとして大変な部分も多く、新たな経験を得ることができました。<br>デザインに関しては、わかりやすくシンプルなものを目指しましたが、専門的な内容を簡潔に伝える難しさに直面し、結果的に長い文章になってしまいました。<br>この経験から、次回同様の課題が発生した際には、図解や視覚的な要素を活用して、コンパクトで分かりやすい形にまとめることの重要性を再認識しました。</dd>
-        </dl>
-      </section>
-    </div>
-  </main>  
-
-<?php get_footer(); ?>
+<?php get_header(); ?>
+  <?php if( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <main class="page-wrap">
+        <article id="post-<?php the_ID(); ?>" <?php post_class( 'article' ); ?>>
+          <h1 class="page-title">
+              <span class="title-en">Works</span>
+              <span class="title-ja">制作物について</span>
+          </h1>
+          <div class="works-page-wrap">
+            <?php the_category(); ?>
+            <h2 class="works-content-title"><?php the_title(); ?></h2>
+             <?php the_content(); ?>
+          </div>
+        </article>
+      </main>
+    <?php endwhile; ?>
+  <?php endif; ?>
+ <?php get_footer(); ?>
