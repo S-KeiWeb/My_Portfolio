60e66470bb41e3d792f2e2acfbb3d98359211c3c
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2023-12-13 10:45:28 +0900

    追従メニュー用のjs追加

--- js/app.js
+++ js/app.js
@@ -5,3 +5,45 @@ jQuery(function (){
   });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const container = document.getElementById('main-container');
+  const aside = document.getElementById('aside');
+  const offsetY = 20;
+
+  if (container && aside) {
+    container.style.minHeight = aside.clientHeight + 'px';
+
+    window.addEventListener('scroll',()=>{
+      const containerRect = container.getBoundingClientRect();
+      const isReachBottom = (aside,containerRect,offsetY)=>{
+        if( aside.clientHeight < window.innerHeight ){
+          if(containerRect.bottom <= aside.clientHeight + offsetY){
+            return true;
+          }else{
+            return false;
+          }
+        }else{
+          if(containerRect.bottom < window.innerHeight - offsetY){
+            return true;
+          }else{
+            return false;
+          }
+        }
+      }
+
+      if( isReachBottom(aside,containerRect,offsetY) ){
+        aside.classList.add('is-end');
+        aside.classList.add('is-fixed');
+        aside.style.left = '';
+      }else if( containerRect.top < offsetY ){
+        aside.classList.add('is-fixed');
+        aside.classList.remove('is-end');
+        aside.style.left = containerRect.left + 'px';
+      }else{
+        aside.classList.remove('is-fixed');
+        aside.classList.remove('is-end');
+        aside.style.left = '';
+      }
+  });
+}
+});
