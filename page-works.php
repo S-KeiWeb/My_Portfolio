6eee5e1ea0dbf959dc38193f5750d70ed14f22d1
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2023-12-14 09:35:39 +0900

    page-title変更

--- page-works.php
+++ page-works.php
@@ -2,15 +2,14 @@
 
   <main class="page-wrap">
     <h1 class="page-title">
-        <span class="title-en"><?php echo strtoupper ($post->post_name); ?></span>
+        <span class="title-en"><?php echo ucwords ($post->post_name); ?></span>
         <span class="title-ja"><?php the_title(); ?></span>
     </h1>
     <section class="works-content-wrap">
-      <h2 class="main-content-title">
-        <span class="main-content-title-en works-responsive-title">Works and Experience</span>
-        <span class="main-content-title-ja">制作物と実績紹介</span>
+      <h2 class="main-section-content-title">
+        <span class="main-section-content-title-en works-responsive-title">Productions</span>
+        <span class="main-section-content-title-ja">制作物</span>
       </h2>
-      <h3 class="works-content-title">制作物</h3>
       <div class="works-image-wrap">
         <?php
           $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
@@ -35,18 +34,22 @@
         <?php endwhile; ?>    
       </div>       
     </section>
-    <section class="works-summary">
-      <h2 class="works-content-title">実績</h2>
+    <section class="works-content-wrap">
+      <h2 class="main-section-content-title">
+        <span class="main-section-content-title-en works-responsive-title">Experience</span>
+        <span class="main-section-content-title-ja">実績紹介</span>
+      </h2>
+      <img class="experience-img" src="<?php echo get_template_directory_uri(); ?>/img/Experience-image.png" alt="coconala画像">
       <p class="works-content-text">今まで仕事の依頼は全てクラウドソーシングサイトから請け負っておりました。<br>その中でも、一番依頼いただいたのがココナラからでした。<br>以下にリンクを貼っておきますので、実績・評価も併せてご覧ください。</p>
-      <a class="mainpage-button" href="https://coconala.com/users/2346005" target="_blank">ココナラ（外部サイト）へ</a>
+      <a class="mainpage-button" href="https://coconala.com/users/2346005" target="_blank">Go To coconala (External Sites)</a>
     </section>
-    <section class="works-contact">
-      <h2 class="main-content-title">
-        <span class="main-content-title-en">Contact</span>
-        <span class="main-content-title-ja">お問い合わせについて</span>
+    <section class="works-content-wrap">
+      <h2 class="main-section-content-title">
+        <span class="main-section-content-title-en">Contact</span>
+        <span class="main-section-content-title-ja">お問い合わせについて</span>
       </h2>
       <p class="works-content-text">ポートフォリオサイトをご覧いただきありがとうございました。<br>業界未経験ではありますが、この転職の機会を通じて新たな挑戦でも貢献できるよう準備を整えております。<br>もし、追加の情報や質問がありましたらどうぞお気軽にお知らせください。<br>最後に、お忙しい中私のポートフォリオサイトにお時間を割いていただき、誠にありがとうございました。</p>
-      <a class="mainpage-button" href="#">お問い合せはこちらから</a>
+      <a class="mainpage-button" href="<?php echo esc_url( home_url( '/' ) ); ?>contact">Please Message</a>
     </section>
   </main>
 <?php endif; ?>
--- page.php
+++ page.php
@@ -4,7 +4,7 @@
 	<?php while ( have_posts() ) : the_post(); ?>
   <main class="page-wrap">
 		<h1 class="page-title">
-        <span class="title-en"><?php echo strtoupper ($post->post_name); ?></span>
+        <span class="title-en"><?php echo ucwords ($post->post_name); ?></span>
         <span class="title-ja"><?php the_title(); ?></span>
     </h1>
 		<div class="container">
