c7373ecf00f67ca1b4e1834b8cc8ec5d9ae19795
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2023-12-20 09:56:24 +0900

    functions.phpにstyleをまとめたので削除

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://unpkg.com/destyle.css@4.0.0/destyle.min.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@300&family=Montserrat&family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
