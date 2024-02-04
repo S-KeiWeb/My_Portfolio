1f67ded63d608f2f85f66afbdb5d8c070df897a2
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2024-02-04 15:17:26 +0900

    profileまで

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ function my_plugin_assets()
   wp_enqueue_style('portfolio-style', get_template_directory_uri() . '/css/style.css');
   wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Gothic+A1:wght@200;300;400&family=Montserrat:wght@100;200;300;400&display=swap');
   wp_enqueue_script('portfolio-main', get_template_directory_uri() . '/js/app.js');
+  wp_enqueue_script('fontawesome', 'https://kit.fontawesome.com/e4895282b1.js');
 }
 
 remove_filter('the_excerpt', 'wpautop');
--- page-about.php
+++ page-about.php
@@ -17,23 +17,15 @@
 					<div class="profile">
 						<div class="profile-img-wrap">
 							<img class="my-img" src="<?php echo get_template_directory_uri(); ?>/img/profile-test.jpg" alt="齊藤恵画像">
-							<div class="name-area">
-								<div class="name-wrap">
-									<p class="my-name-kana">さいとうけい</p>
-									<p class="my-name">齊藤恵</p>
-								</div>
-								<p class="my-name-birth">（1985年2月7日）</p>
-							</div>
-							<p class="my-profile-text">岩手県盛岡市出身、今は奥さんと千葉県野田市に暮らしています。<br>得意なことはひたすら作業する事と、何事にも続けられる継続力です。</p>
 						</div>
 						<div class="profile-text-wrap">
-							<h2 class="profile-title">
-								<span>マネジメント力</span>
-								<span>×</span>
-								<span>プログラミング</span>
-							</h2>
-							<p class="profile-text">
-								私は現在、ドラッグストアの店長として働きながら、副業でWEB制作の仕事を行っています。<br>店長として100人以上の従業員をマネジメントし、幅広い年齢層のチームを指導する中で、常に謙虚な姿勢で接することを心がけてきました。<br>3年前からは独学でプログラミングを学び、その後MENTAのプロの指導を受けてスキルを高めてきました。<br>WEB制作の副業では、経験は浅いですが、WEBデザインやSEOマーケティングのアドバイスなどを通じてクライアントの期待以上の成果を提供できるよう努めてきました。<br>特にココナラでの評価が高く、WEB制作に関する相談も増えています。<br>今は、店舗マネジメントで培ったコミュニケーション能力と、プロから学んだプログラミング技術を活かして、新しい企業で一緒に働く機会を探しています。
+							<div class="name-wrap">
+								<p class="my-name">齊藤恵</p>
+								<p class="my-name-kana">Kei Saitou</p>
+							</div>
+							<p class="profile-text">岩手県盛岡市出身、39歳。<br>
+								現状に満足せず、スキルを磨ける場を求めるため、自らWebスキルを学び、WEB業界へのキャリアチェンジを目指しております。<br>
+								現在、店舗マネジメントで培ったコミュニケーション能力と、自ら学んだWEB制作スキルを活かし、新しい企業で一緒に働く機会を探しています。
 							</p>
 						</div>
 					</div>
@@ -44,6 +36,31 @@
 								<span class="main-section-content-title-en">Skill</span>
 								<span class="main-section-content-title-ja">できること</span>
 							</h2>
+							<div class="web-production-wrap">
+								<div class="web-production-title-wrap">
+									<h2 class="web-production-title">WEB制作スキル</h2>
+								</div>
+								<div class="web-production-contents-wrap">
+									<h3 class="skill-text-title">WEBデザイン</h3>
+									<i class="fa-solid fa-pen-nib"></i>
+									<p>WEBサイトを作る際、準備が大事だと感じています。仕事で学んだ経験を活かし、クライアントの声に耳を傾け、相手の意図や視点を理解・共感するよう心がけています。<br>デザインでは、基本の原則を押さえつつ、クライアントの希望を叶えつつも、使いやすく分かりやすいデザインを目指しています。</p>
+								</div>
+								<div class="web-production-contents-wrap">
+									<h3 class="skill-text-title">WEBデザイン</h3>
+									<i class="fa-regular fa-keyboard"></i>
+									<p>WEBサイトを作る際、準備が大事だと感じています。仕事で学んだ経験を活かし、クライアントの声に耳を傾け、相手の意図や視点を理解・共感するよう心がけています。<br>デザインでは、基本の原則を押さえつつ、クライアントの希望を叶えつつも、使いやすく分かりやすいデザインを目指しています。</p>
+								</div>
+									
+								<p class="skill-text">
+									WEBサイトを立ち上げる際、準備段階が非常に重要だと学びました。<br>そのため、私は現職で培った経験を活かし、クライアントの声に耳を傾け、相手の意図や視点を理解し共感することを心掛けています。<br>また、デザインにおいては、基本的なデザインの原則を把握した上で、クライアントの要望を実現する一方で、エンドユーザーが使いやすい、分かりやすいデザインを追求しています。
+								</p>
+							</div>
+							<div class="skill-text-wrap">
+								<h3 class="skill-text-title">コーディング</h3>
+								<p class="skill-text">
+									HTMLを使用してウェブページの基本構造を作成し、CSSを用いて快適な閲覧環境を調整できます。<br>また、JavaScriptの基礎を理解し、jQueryなどのライブラリを使って動的な要素を持つサイトを制作できます。<br>さらに、HTMLをWordPressテーマとして実装するスキルも持っています。
+								</p>
+							</div>
 							<div class="skill-content">
 								<div class="skill-wrap">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110" class="circle-wrap">
@@ -96,34 +113,21 @@
 									<p class="skill-item-text">Adobe XD</p>
 								</div>
 							</div>
-							<div class="skill-text-wrap">
-								<h3 class="skill-text-title">WEBデザイン</h3>
-								<p class="skill-text">
-									WEBサイトを立ち上げる際、準備段階が非常に重要だと学びました。<br>そのため、私は現職で培った経験を活かし、クライアントの声に耳を傾け、相手の意図や視点を理解し共感することを心掛けています。<br>また、デザインにおいては、基本的なデザインの原則を把握した上で、クライアントの要望を実現する一方で、エンドユーザーが使いやすい、分かりやすいデザインを追求しています。
-								</p>
-							</div>
-							<div class="skill-text-wrap">
-								<h3 class="skill-text-title">コーディング</h3>
-								<p class="skill-text">
-									HTMLを使用してウェブページの基本構造を作成し、CSSを用いて快適な閲覧環境を調整できます。<br>また、JavaScriptの基礎を理解し、jQueryなどのライブラリを使って動的な要素を持つサイトを制作できます。<br>さらに、HTMLをWordPressテーマとして実装するスキルも持っています。
-								</p>
-							</div>
 						</section>
 						<section>
-							<h2 class="main-section-content-title">
-								<span class="main-section-content-title-en">Vision</span>
-								<span class="main-section-content-title-ja">これからのこと</span>
-							</h2>
-							<div class="message-image-wrap">
-								<img class="message-image" src="<?php echo get_template_directory_uri(); ?>/img/message-img.jpg"
-									alt="出身県の風景">
-								<p class="message-image-text">故郷岩手県の風景</p>
-							</div>
-							<p class="message-main-text">
-								最後までご覧いただきありがとうございました。<br>日々楽しく、やりたいことをできるようにする<br>36歳の節目にして初めて人生の目的を見つけました。<br>そのために必要なスキルや知識を獲得するため努力し、日々ITリテラシーの向上に取り組んでいます。<br>私の目標は、自身の技術や知識を活かし、多くの人々がITに親しみを持てるようサポートすることです。<br>私は年齢に負けずに情熱を持ち、会社に求められるだけの価値を提供できると確信しており、ぜひ貴社でのチャンスを頂戴したいと考えています。どうぞよろしくお願いいたします。
-							</p>
+							<div class="profile-text-wrap">
+									<h2 class="profile-title">
+										<span>マネジメント力</span>
+										<span>×</span>
+										<span>プログラミング</span>
+									</h2>
+									<p class="profile-text">
+										私は現在、ドラッグストアの店長として働きながら、副業でWEB制作の仕事を行っています。<br>店長として100人以上の従業員をマネジメントし、幅広い年齢層のチームを指導する中で、常に謙虚な姿勢で接することを心がけてきました。<br>3年前からは独学でプログラミングを学び、その後MENTAのプロの指導を受けてスキルを高めてきました。<br>WEB制作の副業では、経験は浅いですが、WEBデザインやSEOマーケティングのアドバイスなどを通じてクライアントの期待以上の成果を提供できるよう努めてきました。<br>特にココナラでの評価が高く、WEB制作に関する相談も増えています。<br>今は、店舗マネジメントで培ったコミュニケーション能力と、プロから学んだプログラミング技術を活かして、新しい企業で一緒に働く機会を探しています。
+									</p>
+								</div>
 						</section>
 					</div>
+
 				</div>
 				<div id="aside" class="side-menu page-side-menu">
 					<div class="side-menu-title">
