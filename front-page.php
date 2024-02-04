d2d537d1d3478fd73acd283a30b987322635506a
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2024-02-04 10:25:22 +0900

    フォーマッターで自動整形

--- front-page.php
+++ front-page.php
@@ -1,6 +1,8 @@
-<?php get_header(); ?>  
+<?php get_header(); ?>
 
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if (have_posts()):
+  while (have_posts()):
+    the_post(); ?>
 
     <main>
       <section class="top-main-wrap">
@@ -12,96 +14,112 @@
           <div class="top-concept-wrap">
             <h1 class="concept">
               <strong>
-                <span class="main-concept"><?php echo esc_html( get_field('main-concept') ); ?></span>
-                <span class="main-concept-text"><?php echo esc_html( get_field('main-concept-text') ); ?></span>
+                <span class="main-concept">
+                  <?php echo esc_html(get_field('main-concept')); ?>
+                </span>
+                <span class="main-concept-text">
+                  <?php echo esc_html(get_field('main-concept-text')); ?>
+                </span>
               </strong>
             </h1>
           </div>
           <div class="concept-en">
-            <h2 class="main-concept-en"><?php echo esc_html( get_field('main-concept-en') ); ?></h2>
-            <p class="main-concept-message"><?php echo wp_kses_post ( get_field('main-concept-message') ); ?></p>
+            <h2 class="main-concept-en">
+              <?php echo esc_html(get_field('main-concept-en')); ?>
+            </h2>
+            <p class="main-concept-message">
+              <?php echo wp_kses_post(get_field('main-concept-message')); ?>
+            </p>
           </div>
         </div>
       </section>
 
       <div id="main-container" class="main-section-wrap">
-          <div class="main-section-content-wrap">
-            <section class="main-section-content">
-              <h2 class="main-section-content-title">
-                <span class="main-section-content-title-en">About</span>
-                <span class="main-section-content-title-ja">わたしについて</span>
-              </h2>
-              <div class="main-content-wrap">
-                <div class="mainpage-about-image">
-                  <img class="main-content-img fadein" src="<?php echo get_template_directory_uri(); ?>/img/profile-test.jpg" alt="about-img">
-                </div>
-                  <div class="main-about-content">
-                    <div class="main-about-text fadein">
-                    <?php echo wp_kses_post ( get_field('main-about-text') ); ?>
-                    </div>
-                    <div class="mainpage-about-button">
-                      <a class="main-button" href="<?php echo esc_url( home_url( '/' ) ); ?>about">Who is Kei?</a>
-                    </div>
-                  </div>
+        <div class="main-section-content-wrap">
+          <section class="main-section-content">
+            <h2 class="main-section-content-title">
+              <span class="main-section-content-title-en">About</span>
+              <span class="main-section-content-title-ja">わたしについて</span>
+            </h2>
+            <div class="main-content-wrap">
+              <div class="mainpage-about-image">
+                <img class="main-content-img fadein" src="<?php echo get_template_directory_uri(); ?>/img/profile-test.jpg"
+                  alt="about-img">
               </div>
-            </section>
-            <section class="main-section-content">
-              <h2 class="main-section-content-title">
-                <span class="main-section-content-title-en">Works</span>
-                <span class="main-section-content-title-ja">制作物について</span>
-              </h2>
-                <?php
-                  $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-                  $args = array(
-                    'post_type' => 'my-works',
-                    'posts_per_page' => 4,
-                    'paged' => $paged,
-                    'orderby'  => 'date',
-                    'order' => 'ASC'
-                  );
-                  $the_query = new WP_Query( $args );
-                  if($the_query->have_posts() ) : while ($the_query->have_posts()) : $the_query->the_post(); 
+              <div class="main-about-content">
+                <div class="main-about-text fadein">
+                  <?php echo wp_kses_post(get_field('main-about-text')); ?>
+                </div>
+                <div class="mainpage-about-button">
+                  <a class="main-button" href="<?php echo esc_url(home_url('/')); ?>about">Who is Kei?</a>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="main-section-content">
+            <h2 class="main-section-content-title">
+              <span class="main-section-content-title-en">Works</span>
+              <span class="main-section-content-title-ja">制作物について</span>
+            </h2>
+            <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $args = array(
+              'post_type' => 'my-works',
+              'posts_per_page' => 4,
+              'paged' => $paged,
+              'orderby' => 'date',
+              'order' => 'ASC'
+            );
+            $the_query = new WP_Query($args);
+            if ($the_query->have_posts()):
+              while ($the_query->have_posts()):
+                $the_query->the_post();
                 ?>
-              <div class="main-works-image-wrap">
-                <div class="main-works-image">
-                  <?php
+                <div class="main-works-image-wrap">
+                  <div class="main-works-image">
+                    <?php
                     $content = get_the_content();
-                    preg_match_all( '/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $content, $matches );
-                    if ( isset( $matches[1][0] ) ) {
-                      echo '<img src="' . esc_url( $matches[1][0] ) . '" alt="Post Image">';
+                    preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $content, $matches);
+                    if (isset($matches[1][0])) {
+                      echo '<img src="' . esc_url($matches[1][0]) . '" alt="Post Image">';
                     } else {
-                      if ( has_post_thumbnail() ) {
+                      if (has_post_thumbnail()) {
                         the_post_thumbnail();
                       }
                     }
-                  ?>
+                    ?>
+                  </div>
+                  <div class="works-text-wrap">
+                    <p class="works-text">
+                      <?php the_excerpt(); ?>
+                    </p>
+                    <p class="works-image-text">
+                      <?php the_title(); ?>
+                    </p>
+                  </div>
                 </div>
-                <div class="works-text-wrap">
-                  <p class="works-text"><?php the_excerpt(); ?></p>
-                  <p class="works-image-text"><?php the_title(); ?></p>
-                </div> 
-              </div>
-                <?php endwhile; ?>    
-                <?php endif; wp_reset_postdata(); ?>
-              <a class="main-button" href="<?php echo esc_url( home_url( '/' ) ); ?>works">Go to Works Page</a>
-            </section>
+              <?php endwhile; ?>
+            <?php endif;
+            wp_reset_postdata(); ?>
+            <a class="main-button" href="<?php echo esc_url(home_url('/')); ?>works">Go to Works Page</a>
+          </section>
+        </div>
+        <div id="aside" class="side-menu">
+          <div class="side-menu-title">
+            <a href="#">Kei's</a>
+            <a href="#">Portfolio</a>
           </div>
-          <div id="aside" class="side-menu">
-            <div class="side-menu-title">
-              <a href="#">Kei's</a>
-              <a href="#">Portfolio</a>
-            </div>
-            <nav class="side-nav-wrap">
-              <?php
-              $args = array(
-                'menu' => 'side-global-navigation',
-                'menu_class' => 'side-nav',
-                'container' => false,
-              );
-              wp_nav_menu($args);
-              ?>
-            </nav>
-          </div>
+          <nav class="side-nav-wrap">
+            <?php
+            $args = array(
+              'menu' => 'side-global-navigation',
+              'menu_class' => 'side-nav',
+              'container' => false,
+            );
+            wp_nav_menu($args);
+            ?>
+          </nav>
+        </div>
       </div>
     </main>
   <?php endwhile; endif; ?>
--- page-works.php
+++ page-works.php
@@ -1,33 +1,43 @@
-<?php get_header(); ?>  
+<?php get_header(); ?>
 
-  <main class="page-wrap">
-    <h1 class="page-title">
-        <span class="title-en"><?php echo ucwords ($post->post_name); ?></span>
-        <span class="title-ja"><?php the_title(); ?></span>
-    </h1>
-    <div class="works-image-wrap">
-      <?php
-        $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-        $args = array(
-          'post_type' => 'my-works',
-          'posts_per_page' => 4,
-          'paged' => $paged
-        );
-        $the_query = new WP_Query( $args );
-      ?>
-      <?php if($the_query->have_posts() ) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
-      <div class="main-works-image">
-        <a href="<?php the_permalink(); ?>">
-          <?php if ( has_post_thumbnail() ) {
-            the_post_thumbnail();
-          }
-          ?>
-            <p class="works-excerpt"><?php the_excerpt(); ?></p>
-            <p class="works-image-title"><?php the_title(); ?></p>
-        </a>
-      </div>
-      <?php endwhile; ?>    
-    </div>       
+<main class="page-wrap">
+  <h1 class="page-title">
+    <span class="title-en">
+      <?php echo ucwords($post->post_name); ?>
+    </span>
+    <span class="title-ja">
+      <?php the_title(); ?>
+    </span>
+  </h1>
+  <div class="works-image-wrap">
+    <?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $args = array(
+      'post_type' => 'my-works',
+      'posts_per_page' => 4,
+      'paged' => $paged
+    );
+    $the_query = new WP_Query($args);
+    ?>
+    <?php if ($the_query->have_posts()):
+      while ($the_query->have_posts()):
+        $the_query->the_post(); ?>
+        <div class="main-works-image">
+          <a href="<?php the_permalink(); ?>">
+            <?php if (has_post_thumbnail()) {
+              the_post_thumbnail();
+            }
+            ?>
+            <p class="works-excerpt">
+              <?php the_excerpt(); ?>
+            </p>
+            <p class="works-image-title">
+              <?php the_title(); ?>
+            </p>
+          </a>
+        </div>
+      <?php endwhile; ?>
+    </div>
   </main>
 <?php endif; ?>
 
