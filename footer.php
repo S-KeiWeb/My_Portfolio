14470d7c17b743ee0c96e030b7800298f42ee56b
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2024-02-18 16:59:02 +0900

    cta.php追加にともないコンテンツを削除

--- footer.php
+++ footer.php
@@ -1,17 +1,7 @@
 <footer class="footer">
-  <div class="footer-contact-wrap">
-    <p class="footer-message">
-    ポートフォリオサイトをご覧いただきありがとうございました。<br>
-    業界未経験ではありますが、この転職の機会を通じて新たな挑戦でも貢献できるよう準備を整えております。<br>
-    もし、追加の情報や質問がありましたらどうぞお気軽にお知らせください。<br>
-    最後に、お忙しい中私のポートフォリオサイトにお時間を割いていただき、誠にありがとうございました。
-    </p>
-    <div class="footer-contact-button-wrap">
-      <a class="footer-contact-button" href="<?php echo esc_url( home_url( '/' ) ); ?>contact">
-        <span class="footer-contact-button-item">Contact Me</span>
-      </a>
-    </div>
-  </div>
+  <?php if(!is_page('contact')): ?>
+  <?php get_template_part('cta'); ?>
+  <?php endif; ?>
   <nav class="footer-nav-wrap">
     <?php
     $args = array(
