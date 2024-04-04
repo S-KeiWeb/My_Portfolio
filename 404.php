806eab93b6c334f673699ec143404a8db3cdaaaf
Kont-Fi-Fu <jy0nk1k1k0nta1023@gmail.com>
2024-04-04 22:55:32 +0900

    class名変更

--- 404.php
+++ 404.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
-<div class="not-found-page-title-wrap">
-  <h2 class="not-found-page-title">404 NOT FOUND<br><span class="not-found-sub-title">ERROR</span></h2>
+<div class="other-page-title-wrap">
+  <h2 class="other-page-title">404 NOT FOUND<br><span class="other-sub-title">ERROR</span></h2>
 </div>
 
-<div class="not-found-page-main">
-  <div class="not-found-page-section">
+<div class="other-page-main">
+  <div class="other-page-section">
     <p>お探しのページは見つかりませんでした。</p>
     <p>申し訳ございませんが、<a class="return-home" href="<?php echo home_url('/'); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
   </div>
